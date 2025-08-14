865629b7ba2b0b37bf45cfca524cba717571c02e
Seu Nome <seuemail@exemplo.com>
2025-08-14 14:37:40 -0300

    finalizado

---
+++ criacaometodos04/src/criacaometodos04/Criacaometodos04.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package criacaometodos04;
+
+/**
+ *
+ * @author 2024122760121
+ */
+public class Criacaometodos04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Somando "+ Calc.somar(3, 4));
+        System.out.println("Subtraindo "+ Calc.subtrair(3, 4));
+        System.out.println("Multiplicando "+ Calc.multiplicar(3, 4));
+        System.out.println("Dividindo "+ Calc.dividir(3, 4));
+    }
+    
+}
